50e9be78d6c8e9346d077486ccf59738f582ee1c
CristianX <thecristianx@hotmail.com>
2021-05-11 14:25:41 -0500

    Creando Floating Action Buttons con su funcionalidad

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -11,9 +11,19 @@ export const ContadorScreen = () => {
                 Contador: {contador}
             </Text>
 
-            <TouchableOpacity onPress={ () => setContador( contador +1 ) } >
-                <View style={styles.botonIncrementar}>
-                    <Text>+1</Text>
+            <TouchableOpacity style={ styles.fabLocationBR } onPress={ () => setContador( contador +1 ) } >
+                <View style={ styles.fab } >
+                    <Text style={ styles.fabText }>
+                        +1
+                    </Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={ styles.fabLocationBL } onPress={ () => setContador( contador -1 ) } >
+                <View style={ styles.fab } >
+                    <Text style={ styles.fabText }>
+                        -1
+                    </Text>
                 </View>
             </TouchableOpacity>
 
@@ -25,16 +35,41 @@ export const ContadorScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
     }, 
     title: {
-        textAlign: 'center', 
         fontSize: 40, 
-        top:  -15 
+        textAlign: 'center', 
+        top:  -15,
     },
 
-    botonIncrementar: {
-        backgroundColor: 'red',
-        borderRadius: 100 
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25,
+    },
+
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25,
+    },
+
+    fab: {
+        backgroundColor: '#5856d6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+
     }
+
+
 })
